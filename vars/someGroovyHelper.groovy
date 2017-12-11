11edca6ed55fbd7de5c946d6a1fb0528f3291380
Thiago Retondar <t.retondar@gmail.com>
2017-12-10 23:19:28 -0200

    new class instance

--- vars/someGroovyHelper.groovy
+++ vars/someGroovyHelper.groovy
@@ -2,5 +2,7 @@ import org.thiagoretondar.HelperTest
 
 def call(someStringParam) {
     echo "CALLING HELPER TEST WITH ${someStringParam}"
-    HelperTest(someStringParam)
+
+    HelperTest helperTest = new HelperTest()
+    helperTest.doSomething(someStringParam)
 }
